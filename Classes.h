a3879d30d04325dc07e3d9e577daf3fc0f28dfbb
Alex <masala.alex13@gmail.com>
2020-12-26 11:45:16 +0200

    modified class

--- Classes.h
+++ Classes.h
@@ -7,11 +7,11 @@ class ColumnLayout
 {
 	const int nrInregistrare;
 	char* nume_coloana;
-	string tip_coloana;
+	char* tip_coloana;
 	int* dimensiune_coloana;
-	int nrColoane;
-	string valoare_implicita;
+	char* valoare_implicita;
 	int is_nullable;
+	int nrColoane;
 	static string descriere_coloana;
 
 	//	string filtru; /*  yes/no  */
@@ -37,19 +37,18 @@ public:
 		strcpy_s(this->nume_coloana, strlen(nouNume_coloana) + 1, nouNume_coloana);
 	}
 
-	string getTipColoana()
+	char* getTipColoana()
 	{
 		return this->tip_coloana;
 	}
 
-	void setTipColoana(string tip_coloana)
+	void setTipColoana(const char* nouTip_coloana)
 	{
-		if (tip_coloana.size() >= 4)
-		{
-			this->tip_coloana = tip_coloana;
+		if (this->tip_coloana != nullptr) {
+			delete[] this->tip_coloana;
 		}
-		else
-			this->tip_coloana = "Necunoscut";
+		this->tip_coloana = new char[strlen(nouTip_coloana) + 1];
+		strcpy_s(this->tip_coloana, strlen(nouTip_coloana) + 1, nouTip_coloana);
 	}
 
 	int* getDimensiuneColoana()
@@ -69,22 +68,20 @@ public:
 		for (int i = 0; i < nouNrColoane; i++) {
 			this->dimensiune_coloana[i] = nouaDimensiune_coloana[i];
 		}
-
 	}
 
-	string getValoareImplicita()
+	char* getValoareImplicita()
 	{
 		return this->valoare_implicita;
 	}
 
-	void setValoareImplicita(string valoare_implicita)
+	void setValoareImplicita(const char* nouaValoare_implicita)
 	{
-		if (valoare_implicita.size() != 0)
-		{
-			this->valoare_implicita = valoare_implicita;
+		if (this->valoare_implicita != nullptr) {
+			delete[] this->valoare_implicita;
 		}
-		else
-			this->valoare_implicita = "Necunoscuta";
+		this->valoare_implicita = new char[strlen(nouaValoare_implicita) + 1];
+		strcpy_s(this->valoare_implicita, strlen(nouaValoare_implicita) + 1, nouaValoare_implicita);
 	}
 
 	int getIsNullable()
@@ -119,9 +116,9 @@ public:
 		this->setDescriereColoana("");;
 	}
 
-	ColumnLayout(int nrInregistrare, char* nume_coloana, string tip_coloana,int* dimensiune_coloana, int nrColoane,string valoare_implicita) :nrInregistrare(nrInregistrare) {
+	ColumnLayout(int nrInregistrare, char* nume_coloana, char* tip_coloana,int* dimensiune_coloana, int nrColoane, char* valoare_implicita) :nrInregistrare(nrInregistrare) {
 		this->setNumeColoana(nume_coloana);
-		this->setTipColoana(tip_coloana);;
+		this->setTipColoana(tip_coloana);
 		this->setDimensiuneColoana(dimensiune_coloana, nrColoane);
 		this->setValoareImplicita(valoare_implicita);
 		/*this->setIsNullable(0);
