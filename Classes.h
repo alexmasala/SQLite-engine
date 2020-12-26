86f9d81b2a0ff0f08a6b8f9ba3cd3291b025e5a4
moldovanalexandra <alexandradianamoldovan@gmail.com>
2020-12-27 00:20:48 +0200

    operators overloading

--- Classes.h
+++ Classes.h
@@ -27,7 +27,6 @@ public:
 	{
 		return this->nume_coloana;
 	}
-
 	void setNumeColoana(const char* nouNume_coloana)
 	{
 
@@ -56,7 +55,10 @@ public:
 	{
 		return this->dimensiune_coloana;
 	}
-
+	int getDimensiuneColoana(int i)
+	{
+		return dimensiune_coloana[i];
+	}
 	void setDimensiuneColoana(int* nouaDimensiune_coloana, int nouNrColoane)
 	{
 
@@ -127,7 +129,7 @@ public:
 		this->setDescriereColoana("");
 	}
 
-	ColumnLayout(int nrInregistrare,const char* nume_coloana, char* tip_coloana,int* dimensiune_coloana, int nrColoane, char* valoare_implicita) :nrInregistrare(nrInregistrare) {
+	ColumnLayout(int nrInregistrare,const char* nume_coloana,const char* tip_coloana,int* dimensiune_coloana, int nrColoane,const char* valoare_implicita) :nrInregistrare(nrInregistrare) {
 		this->setNumeColoana(nume_coloana);
 		this->setNrColoane(nrColoane);
 		this->setTipColoana(tip_coloana);
@@ -168,6 +170,46 @@ public:
 		}
 	}
 
+	bool operator!()
+	{
+		return nrColoane > 0;
+	}
+
+	ColumnLayout operator++()
+	{
+		this->nrColoane++;
+		return *this;
+	}
+	ColumnLayout operator++(int i)
+	{
+		ColumnLayout aux = *this;
+		this->nrColoane++;
+		return aux;
+	}
+	explicit operator int()
+	{
+		return nrInregistrare;
+	}
+	bool operator== (ColumnLayout& cl)
+	{
+		bool same = false;
+		if (strcmp(this->nume_coloana, cl.nume_coloana) != 0)
+		{
+			same = true;
+		}
+		return same;
+	}
+	bool operator<(ColumnLayout& cl)
+	{
+		bool less = false;
+		if (this->nrInregistrare < cl.nrInregistrare)
+		{
+			less = true;
+		}
+		return less;
+	}
+
+	friend istream& operator>>(istream&, ColumnLayout&);
 	friend ostream& operator<<(ostream&, ColumnLayout);
 };
 
@@ -180,6 +222,11 @@ ostream& operator<<(ostream& out, ColumnLayout c)
 	cout << "Nr. coloane: ";
 	out << c.getNrColoane() << endl;
 	cout << "Dimensiuni per coloana: ";
+	for (int i = 0; i < c.getNrColoane(); i++)
+	{
+		out << c.getDimensiuneColoana(i) << " ";
+	}
+	out << endl;
 	if (c.getNumeColoana() != nullptr)
 	{
 		for (int i = 0; i < c.getNrColoane(); i++)
@@ -188,6 +235,46 @@ ostream& operator<<(ostream& out, ColumnLayout c)
 		}
 	}
 	return out;
+}
+
+istream& operator>>(istream& in, ColumnLayout& c)
+{
+	cout << "Nume coloana: ";
+	char input[100];
+	in.getline(input, 101);
+	c.nume_coloana = new char[strlen(input) + 1];
+	strcpy_s(c.nume_coloana, strlen(input) + 1, input);
+	cout << "Tip coloana: ";
+	in.getline(input, 101);
+	c.tip_coloana = new char[strlen(input) + 1];
+	strcpy_s(c.tip_coloana, strlen(input) + 1, input);
+	cout << "Valoare implicita: ";
+	in.getline(input, 101);
+	c.valoare_implicita = new char[strlen(input) + 1];
+	strcpy_s(c.valoare_implicita, strlen(input) + 1, input);
+	cout << "poate fi null: ";
+	in >> c.is_nullable;
+	cout << "nr coloane: ";
+	in >> c.nrColoane;
+	if (c.dimensiune_coloana != nullptr)
+	{
+		delete[] c.dimensiune_coloana;
+	}
+	if (c.nrColoane > 0)
+	{
+		c.dimensiune_coloana = new int[c.nrColoane];
+		for (int i = 0; i < c.nrColoane; i++)
+		{
+			cout << "Dimensiune " << i + 1 << " ";
+			in >> c.dimensiune_coloana[i];
+		}
+	}
+	else
+	{
+		c.dimensiune_coloana = nullptr;
+	}
+
+	return in;
 }
 
 class Table
--- POO_Project.cpp
+++ POO_Project.cpp
@@ -8,5 +8,28 @@ int main()
 	//TO-DO
 	ColumnLayout column;
 
+	//cout << column;
+
+
+	//ColumnLayout column;
+	int* c1 = new int[3]{ 1,2,3 };
+	ColumnLayout cl1(2, "nume", "text", c1, 3, "necunoscut");
+	int* c2 = new int[2]{ 3,2 };
+	ColumnLayout cl2(3, "varsta", "number", c2, 2, "0");
+
+
+	cout << !cl1 << endl;
+	bool x = cl1 < cl2;
+	cout << x << endl;
+	bool s = cl1 == cl2;
+	cout << s << endl;
+	int nr = (int)cl1;
+	cout << nr << endl;
+	ColumnLayout coloana = ++column;
+	cout << coloana.getNrColoane() <<endl;
+
+	ColumnLayout cl4;
+	cin >> cl4;
+	cout << cl4;
 	cout << column;
 }
