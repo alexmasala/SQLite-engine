1a8128be6a3753c5d1df3e1e9d145692fd90b5dc
Alex <masala.alex13@gmail.com>
2020-12-27 19:30:26 +0200

    + setters, getters, operators for Table class

--- Classes.h
+++ Classes.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 class ColumnLayout
@@ -11,8 +12,9 @@ class ColumnLayout
 	int* dimensiune_coloana = NULL;
 	char* valoare_implicita = NULL;
 	int is_nullable;
-	int nrColoane;
+	int nrColoane = 0;
 	static string descriere_coloana;
+	int x; 
 
 	//	string filtru; /*  yes/no  */
 	//	int coloana_filtru; /*  id cu valoare - ex: pt SELECT(nume) FROM studenti WHERE id = 1 */
@@ -42,7 +44,7 @@ public:
 		return this->tip_coloana;
 	}
 
-	void setTipColoana(const char* nouTip_coloana) //error
+	void setTipColoana(const char* nouTip_coloana) 
 	{
 		if (this->tip_coloana != nullptr) {
 			delete[] this->tip_coloana;
@@ -120,6 +122,9 @@ public:
 		ColumnLayout::descriere_coloana = descriere_coloana;
 	}
 
+	void setX(int i) { x = i; }
+	int getX() { return x; }
+
 	ColumnLayout():nrInregistrare(1) {
 		this->setNumeColoana("");
 		this->setTipColoana("");
@@ -168,6 +173,16 @@ public:
 		{
 			delete[] valoare_implicita;
 		}
+	}
+
+	ColumnLayout& operator=(ColumnLayout& c)
+	{
+		this->setNumeColoana(c.nume_coloana);
+		this->setNrColoane(c.nrColoane);
+		this->setTipColoana(c.tip_coloana);
+		this->setDimensiuneColoana(c.dimensiune_coloana, c.nrColoane);
+		this->setValoareImplicita(c.valoare_implicita);
+		return *this;
 	}
 
 	bool operator!()
@@ -219,6 +234,8 @@ ostream& operator<<(ostream& out, ColumnLayout c)
 {
 	cout << "Nr inregistrare: ";
 	out << c.getNrInregistrare() << endl;
+	cout << "Numele tabelei: ";
+	out << c.getNumeColoana() << endl;
 	cout << "Nr. coloane: ";
 	out << c.getNrColoane() << endl;
 	cout << "Dimensiuni per coloana: ";
@@ -279,14 +296,242 @@ istream& operator>>(istream& in, ColumnLayout& c)
 
 class Table
 {
-	char* nume_tabela;
-	const int capacitate_tabela;
-	//const int nrColoane;
-	ColumnLayout* columns;
+	char* nume_tabela = NULL;
+	int capacitate_tabela;
+//	int nrTotalColoane;
+	ColumnLayout* columns = nullptr;
 	static string descriere_tabela;
+
+public:
+	const char* getNumeTabela()
+	{
+		return this->nume_tabela;
+	}
+	void setNumeTabela(const char* nouNume_tabela)
+	{
+
+		if (this->nume_tabela != nullptr) {
+			delete[] this->nume_tabela;
+		}
+		this->nume_tabela = new char[strlen(nouNume_tabela) + 1];
+		strcpy_s(this->nume_tabela, strlen(nouNume_tabela) + 1, nouNume_tabela);
+	}
+
+	int getCapacitateTabela()
+	{
+		return this->capacitate_tabela;
+	}
+
+	void setCapacitateTabela(int capacitate_tabela)
+	{
+		this->capacitate_tabela = capacitate_tabela;
+	}
+
+	/*int getNrTotalColoane()
+	{
+		return this->nrTotalColoane;
+	}
+
+	void setNrTotalColoane(ColumnLayout* c)
+	{
+		this->nrTotalColoane = c->getNrColoane();
+	}*/
+
+	ColumnLayout* getColumnLayout()
+	{
+		return this->columns;
+	}
+
+	ColumnLayout getColumnLayout(int i)
+	{
+		return columns[i];
+
+		// return dimensiune_coloana[i];
+	}
+
+	void setColumnLayout(ColumnLayout* c)
+	{
+		
+		this->columns = c;
+		//c->setNumeColoana(NULL);
+
+		
+	}
+
+	//void setColumnLayout(ColumnLayout* c)
+	//{
+	//	if (c != nullptr) {
+	//		delete[] c;
+	//	}
+	//	std::vector<ColumnLayout> v;
+
+	//	c = new ColumnLayout[this->getNrTotalColoane()];
+	//	for (int i = 0; i < this->getNrTotalColoane(); i++)
+	//		v.push_back(*c);
+
+	//	//this->columns = c;
+
+	//	//if (this->columns != nullptr) {
+	//	//	delete[] this->columns;
+	//	//}
+	//	////this->columns->setNrColoane(c->getNrColoane());
+
+	//	//this->columns = new ColumnLayout[c->getNrColoane()];
+	//	//for (int i = 0; i < c->getNrColoane(); i++)
+	//	//this->columns[i] = c[i];
+	//}
+
+	static string getDescriereTabela()
+	{
+		return descriere_tabela;
+	}
+
+	static void setDescriereTabela(string descriere_tabela)
+	{
+		Table::descriere_tabela = descriere_tabela;
+	}
+
+	Table() 
+	{
+		this->setNumeTabela("");
+		this->setCapacitateTabela(0);
+		this->setColumnLayout(nullptr);
+	}
+
+	Table(const char* nume_tabela, int capacitate_tabela, ColumnLayout* columns) 
+	{
+		this->setNumeTabela(nume_tabela);
+		this->setCapacitateTabela(capacitate_tabela);
+		this->setColumnLayout(columns);
+	}
+	
+	Table(const Table& t)
+	{
+		this->setNumeTabela(t.nume_tabela);
+		this->setCapacitateTabela(t.capacitate_tabela);
+		this->setColumnLayout(t.columns);
+		this->setDescriereTabela(t.descriere_tabela);
+	}
+	
+	~Table()
+	{
+		if (nume_tabela != nullptr)
+		{
+			delete[] nume_tabela;
+		}
+
+		/*if (columns != nullptr)
+		{
+			delete[] columns;
+		}*/
+	}
+
+	Table& operator=(Table& t)
+	{
+		this->setNumeTabela(t.nume_tabela);
+		this->setCapacitateTabela(t.capacitate_tabela);
+		this->setColumnLayout(t.columns);
+		this->setDescriereTabela(t.descriere_tabela);
+		return *this;
+	}
+
+	explicit operator string()
+	{
+		return descriere_tabela;
+	}
+
+	//--pre
+	Table operator--()
+	{
+		this->capacitate_tabela--;
+		return *this;
+	}
+
+	//--post
+	friend Table operator--(Table& t, int);
+
+	bool operator!()
+	{
+		return columns == nullptr;
+	}
+
+	bool operator>(Table& t)
+	{
+		if (this->getCapacitateTabela() > t.getCapacitateTabela())
+			return true;
+		else
+			return false;
+	}
+
+	bool operator==(Table& t)
+	{
+		if(strcmp(this->getNumeTabela(), t.getNumeTabela()) != 0)
+			return true;
+		else
+			return false;
+	}
+
+	friend istream& operator>>(istream&, Table&);
+	friend ostream& operator<<(ostream&, Table);
 };
 
 string Table::descriere_tabela = "Tables are database objects that contain all the data in a database. In tables, data is logically organized in a row-and-column format similar to a spreadsheet.";
+
+//overloading operator--
+Table operator--(Table& t1, int)
+{
+	Table table = t1;
+	t1.capacitate_tabela = t1.capacitate_tabela--;
+	return t1;
+}
+
+ostream& operator<<(ostream& out, Table t)
+{
+	cout << "Numele tabelei: ";
+	out << t.getNumeTabela() << endl;
+	cout << "Capacitate: ";
+	out << t.getCapacitateTabela() << endl;
+	cout << "Nr. coloane: ";
+	out << t.getColumnLayout()->getNrColoane() << endl;
+	cout << "Coloane: " << endl;
+
+	// EXCEPTION IS THROWN
+	/*cout << "Nume per coloana: " << endl;
+	for (int i = 0; i < t.getColumnLayout()->getNrColoane(); i++)
+	{
+		out << t.getColumnLayout()[i].getNumeColoana() << " ";
+	}*/
+
+	// EXCEPTION IS THROWN
+	/*cout << "Nume per coloana: " << endl;
+	for (int i = 0; i < t.getColumnLayout()->getNrColoane(); i++)
+	{
+		out << t.getColumnLayout()[i].getNumeColoana() << " ";
+	}*/
+
+	cout << "Dimensiune per coloana: " << endl;
+	for (int i = 0; i < t.getColumnLayout()->getNrColoane(); i++)
+	{
+		out << t.getColumnLayout()->getDimensiuneColoana()[i] << " ";
+	}
+
+	out << endl;
+
+	return out;
+}
+
+istream& operator>>(istream& in, Table& t)
+{
+	cout << "Numele tabelei: ";
+	char input[100];
+	in.getline(input, 101);
+	t.nume_tabela = new char[strlen(input) + 1];
+	strcpy_s(t.nume_tabela, strlen(input) + 1, input);
+	cout << "Capacitate: ";
+	in >> t.capacitate_tabela;
+	return in;
+}
+
 
 class Database
 {
@@ -296,21 +541,36 @@ class Database
 	static string descriere_DB;
 };
 
-//coloana de tip integer
-class IntegerColumn : public ColumnLayout
-{
-	int* celule_integer;
+string Database::descriere_DB = "A database is an organized collection of structured information, or data, typically stored electronically in a computer system.";
+
+
+class Commands {
+	static string descriere_comenzi;
 };
 
-//coloana de tip float
-class FloatColumn : public ColumnLayout
-{
-	float* celule_float;
-};
+string Commands::descriere_comenzi = "SQL commands can be used to search the database and to do other functions like creating tables, adding data to tables, modifying data, and dropping tables.";
 
-//coloana de tip text
-class TextColumn : public ColumnLayout
-{
-	char* celule_text;
-};
+class Select_Command {};
+class Insert_Command {};
+class Delete_Command {};
+class Update_Command {};
+
+
+////coloana de tip integer
+//class IntegerColumn : public ColumnLayout
+//{
+//	int* celule_integer;
+//};
+//
+////coloana de tip float
+//class FloatColumn : public ColumnLayout
+//{
+//	float* celule_float;
+//};
+//
+////coloana de tip text
+//class TextColumn : public ColumnLayout
+//{
+//	char* celule_text;
+//};
 
--- POO_Project.cpp
+++ POO_Project.cpp
@@ -9,14 +9,11 @@ int main()
 	ColumnLayout column;
 
 	//cout << column;
-
-
 	//ColumnLayout column;
 	int* c1 = new int[3]{ 1,2,3 };
 	ColumnLayout cl1(2, "nume", "text", c1, 3, "necunoscut");
 	int* c2 = new int[2]{ 3,2 };
 	ColumnLayout cl2(3, "varsta", "number", c2, 2, "0");
-
 
 	cout << !cl1 << endl;
 	bool x = cl1 < cl2;
@@ -32,4 +29,35 @@ int main()
 	cin >> cl4;
 	cout << cl4;
 	cout << column;
+
+	ColumnLayout* obs= new ColumnLayout[2]{cl1,cl2};
+	cout << obs[0]<<endl;
+	cout << obs[1]<<endl;
+
+	Table test("nume",11,obs);
+	cout << test; 
+	Table test1;
+	cin >> test1;
+
+	for (int i = 0; i < 2; i++)
+		test.getColumnLayout()[i].getNumeColoana();
+
+	Table t;
+	t.setNumeTabela("Studenti");
+	t.setCapacitateTabela(15);
+	string str = (string)t;
+	cout << str << endl;
+	if (!t)
+	cout << "Tabela " << t.getNumeTabela() << " nu are coloane!" << endl;
+	
+	Table t1;
+	t1.setNumeTabela("Medicamente");
+	t1.setCapacitateTabela(20);
+	if (t1 > t)
+		cout  << "Tabela " << t1.getNumeTabela() << " are o capacitate mai mare decat tabela " << t.getNumeTabela()<<endl;
+	
+	Table t2;
+	t1.setNumeTabela("Studenti");
+	if (t == t2)
+		cout << "Tabelele au acelasi nume!" <<endl;
 }
